b140c090730d435c8ffcc174b30985c00d874497
Ranger-HK <maxravindu99@gmail.com>
2022-10-27 00:21:43 +0530

    Registration user

--- Simple_Crud_Project/src/main/java/com/mycompany/simple_crud_project/dao/RegistrationDao.java
+++ Simple_Crud_Project/src/main/java/com/mycompany/simple_crud_project/dao/RegistrationDao.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.simple_crud_project.controller;
+package com.mycompany.simple_crud_project.dao;
 
 /**
  *
  * @author ravindu
  */
-public class RegistrationController {
+public class RegistrationDao {
     
 }
